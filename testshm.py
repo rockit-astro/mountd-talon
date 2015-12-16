c319ef32f2fa870ceaf4fc211b50c7a2aca7b823
Paul Chote <p.chote@warwick.ac.uk>
2015-12-16 12:15:24 +0000

    Read homing/limiting/homed flags from shm.

--- testshm.py
+++ testshm.py
@@ -24,6 +24,7 @@ SHM_COVER_STATE_OFFSET = 976
 SHM_RA_AXIS = 248
 SHM_DEC_AXIS = SHM_RA_AXIS + 120
 SHM_FOCUS_AXIS = SHM_RA_AXIS + 360
+SHM_AXIS_FLAGS = 1
 SHM_AXIS_CPOS = 96
 SHM_AXIS_STEP = 4
 SHM_AXIS_DF = 80
@@ -36,6 +37,9 @@ def shm_read_double(data, offset):
 def shm_read_int(data, offset):
     return struct.unpack_from('i', data.read(4, offset))[0]
 
+def shm_read_ushort(data, offset):
+    return struct.unpack_from('H', data.read(2, offset))[0]
+
 data = sysv_ipc.SharedMemory(TALON_SHM_KEY)
 print('mjd: {}'.format(shm_read_double(data, SHM_MJD_OFFSET)))
 print('ra: {}'.format(shm_read_double(data, SHM_RA_OFFSET)))
@@ -43,15 +47,27 @@ print('dec: {}'.format(shm_read_double(data, SHM_DEC_OFFSET)))
 print('tel_state: {}'.format(shm_read_int(data, SHM_TEL_STATE_OFFSET)))
 print('cover_state: {}'.format(shm_read_int(data, SHM_COVER_STATE_OFFSET)))
 
+ra_flags = shm_read_ushort(data, SHM_RA_AXIS + SHM_AXIS_FLAGS)
 print('ra axis:')
+print('   homing: {}'.format(ra_flags & 0x80 != 0))
+print('   limiting: {}'.format(ra_flags & 0x100 != 0))
+print('   ishomed: {}'.format(ra_flags & 0x200 != 0))
 print('   poslim: {}'.format(shm_read_double(data, SHM_RA_AXIS + SHM_AXIS_POSLIM)))
 print('   neglim: {}'.format(shm_read_double(data, SHM_RA_AXIS + SHM_AXIS_NEGLIM)))
 
+dec_flags = shm_read_ushort(data, SHM_DEC_AXIS + SHM_AXIS_FLAGS)
 print('dec axis:')
+print('   homing: {}'.format(dec_flags & 0x80 != 0))
+print('   limiting: {}'.format(dec_flags & 0x100 != 0))
+print('   ishomed: {}'.format(dec_flags & 0x200 != 0))
 print('   poslim: {}'.format(shm_read_double(data, SHM_DEC_AXIS + SHM_AXIS_POSLIM)))
 print('   neglim: {}'.format(shm_read_double(data, SHM_DEC_AXIS + SHM_AXIS_NEGLIM)))
 
+focus_flags = shm_read_ushort(data, SHM_FOCUS_AXIS + SHM_AXIS_FLAGS)
 print('focus axis:')
+print('   homing: {}'.format(focus_flags & 0x80 != 0))
+print('   limiting: {}'.format(focus_flags & 0x100 != 0))
+print('   ishomed: {}'.format(focus_flags & 0x200 != 0))
 print('   cpos: {}'.format(shm_read_double(data, SHM_FOCUS_AXIS + SHM_AXIS_CPOS)))
 print('   step: {}'.format(shm_read_int(data, SHM_FOCUS_AXIS + SHM_AXIS_STEP)))
 print('   df: {}'.format(shm_read_double(data, SHM_FOCUS_AXIS + SHM_AXIS_DF)))
